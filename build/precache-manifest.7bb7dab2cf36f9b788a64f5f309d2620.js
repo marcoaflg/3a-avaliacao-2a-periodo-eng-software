f4753181c6b32a5f89ae84b53653d767b22d41fd
Marco Antônio Felipe de Lima Garcia <marcosl19@gmail.com>
2020-05-11 08:46:06 -0300

    first commit

---
+++ build/precache-manifest.7bb7dab2cf36f9b788a64f5f309d2620.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03922ccb8cc597d38422b5d7a9b205c2",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "78e0b4cae41efa7028c5",
+    "url": "/react-gh-pages/static/css/main.8ee538cb.chunk.css"
+  },
+  {
+    "revision": "2f891778ac9eb49d8d66",
+    "url": "/react-gh-pages/static/js/2.16bf870b.chunk.js"
+  },
+  {
+    "revision": "78e0b4cae41efa7028c5",
+    "url": "/react-gh-pages/static/js/main.51d96466.chunk.js"
+  },
+  {
+    "revision": "7e4cb4093acd77796c7a",
+    "url": "/react-gh-pages/static/js/runtime~main.53d7fbed.js"
+  }
+]);
